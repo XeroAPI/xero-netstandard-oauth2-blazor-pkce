f7ff21aacca17ce8c6a9815af409e0afc214689e
Albert-Xero <albert.gromek@xero.com>
2021-04-20 16:48:51 +1000

    Code working for getting org Name on login tab, but can't display it in html for some reason. Also need to work on storing tenantId.

--- WebApplication1/Server/Controllers/XeroController.cs
+++ WebApplication1/Server/Controllers/XeroController.cs
@@ -26,20 +26,40 @@ namespace WebApplication1.Server.Controllers
             var FilesApi = new FilesApi();
             var accessToken = Request.Headers[HeaderNames.Authorization].ToString().Replace("Bearer ", string.Empty);
             var bearerToken = Request.Headers[HeaderNames.Authorization];
-            Console.WriteLine(accessToken);
-            var handler = new JwtSecurityTokenHandler();
-            var token = handler.ReadJwtToken(accessToken);
+            var tenantId = await GetTenantId(accessToken);
+            var response = await FilesApi.GetFilesAsync(accessToken, tenantId);
+            var filesItems = response.Items;
+            Console.WriteLine("hello");
+            return Ok(filesItems);
+        }
+
+        [HttpGet]
+        [Route("getconnection")]
+        public async Task<IActionResult> GetConnection()
+        {
+            var FilesApi = new FilesApi();
+            var accessToken = Request.Headers[HeaderNames.Authorization].ToString().Replace("Bearer ", string.Empty);
+            var bearerToken = Request.Headers[HeaderNames.Authorization];
+            var tenantId = GetTenantId(accessToken);
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                List<Connection> connections = await client.GetFromJsonAsync<List<Connection>>("https://api.xero.com/connections");
+                return Ok(connections.FirstOrDefault());
+            }
+        }
+
+        public async Task<string> GetTenantId(string accessToken)
+        {
             using (HttpClient client = new HttpClient())
             {
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
                 List<Connection> connections = await client.GetFromJsonAsync<List<Connection>>("https://api.xero.com/connections");
                 var tenantId = connections.FirstOrDefault().tenantId;
-                var response = await FilesApi.GetFilesAsync(accessToken, tenantId);
-                var filesItems = response.Items;
-                Console.WriteLine("hello");
-                return Ok(filesItems);
+                return tenantId;
             }
         }
-
     }
+
 }
---
+++ WebApplication1/Shared/OrgDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Shared
+{
+    public class OrgDetails
+    {
+        public Guid TenantId { get; set; }
+        public string TenantName { get; set; }
+    }
+}
